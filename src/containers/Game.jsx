d11eb1c09d93d13ccb24f47dd30c699c9d65b66d
unknown <giorgiapridonidze08@gmail.com>
2025-08-16 11:55:37 +0400

    finnished handleInput function + created basic of LetterValidate function (startPoint of LEtterValidate) in Game.jsx

--- src/containers/Game.jsx
+++ src/containers/Game.jsx
@@ -10,6 +10,7 @@ const Game = ( { words } ) => {
 
     const [input,setInput] = useState('')
     const [inserted,setInserted] = useState([])
+    const [target,setTarget] = useState(0)
 
 
 
@@ -23,31 +24,67 @@ const Game = ( { words } ) => {
 
 
     useEffect(() => {
+
         const handleInput = (e) => {
 
             const regex = /^[a-zA-Z]+$/
 
 
-            if(e.key === 'Enter'){
+            if(e.key === 'Enter' || e.key === ' '){
+
+                if(input.trim().length > 0){
+                    
                 setInserted(inserted => [...inserted, input])
                 setInput('')
+                setTarget(target => target + 1)
                 return
-            }else if (e.key === 'Backspace'){
+                }else {
+                    return
 
-                setInput(input.slice(0, -1))
-                return
+                }
 
-            }else if (e.key === ' '){
-                setInserted(inserted => [...inserted, input])
-                setInput('')
-                return
-            }else if (e.key > 1){
+
+            }else if (e.key === 'Backspace' ){
+
+
+                
+
+                if (input.length > 0){
+                    setInput(input.slice(0 , -1))
+                }
+
+
+                if(input.length < 1){
+                    setInput('')
+
+
+                    if(inserted.length> 0){
+
+                    setInserted(inserted.slice(0,-1))
+                    setInput(inserted[inserted.length - 1])
+                    setTarget(target => target = (inserted.length - 1))
+                }
+
+
+                if(target < 1){
+                    setTarget(0)
+                    return
+                }
+
+                    return
+                }
+
+                
+
+            }else if (e.key.length > 1){
                 return
             }else if (regex.test(e.key) === false){
                 return
             }
             else {
                 setInput(input => input + e.key)
+
+
 
             }
 
@@ -62,9 +99,31 @@ const Game = ( { words } ) => {
         window.addEventListener('keydown', handleInput)
         return () => {window.removeEventListener('keydown',handleInput)}
 
-    },[input])
+    },[input,target,inserted])
 
  
+        const LetterValidate = () => {
+
+            const inputLastLetter = input[input.length - 1]
+            const targetLetter = words[target][input.length -1]
+
+
+
+            if(!inputLastLetter && !targetLetter){
+                return
+            }
+
+
+             if(inputLastLetter == targetLetter){
+                console.log('true letters ' + inputLastLetter, targetLetter)
+            }else {
+                console.log('false letters ' + inputLastLetter, targetLetter)
+            }
+
+
+        }
+
+LetterValidate()
 
     return(
         <div className="game-container">
