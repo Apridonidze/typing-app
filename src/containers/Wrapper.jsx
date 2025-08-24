53109bb9155e6de8654adfc2ae1f69b2244f06e3
unknown <giorgiapridonidze08@gmail.com>
2025-08-24 23:25:45 +0400

    created blur on unfocused (have to finish)

--- src/containers/Wrapper.jsx
+++ src/containers/Wrapper.jsx
@@ -5,7 +5,7 @@ import Input from "../components/Input"
 const Wrapper = ( { words, wordRef, inserted, input,letterRef,targetLetter,handleRedo, isFocused} ) => {
 
     return(
-        <div className="wrapper-container container py-2 my-blurred-element">
+        <div className="wrapper-container container py-2" style={!isFocused ? {filter : 'blur(5px)'} : {filter : 'blur(0px)'}}>
             <Words words={words} wordRef={wordRef} />
             <Inserted  inserted={inserted}/>
             <Input input={input} letterRef={letterRef} targetLetter={targetLetter}  handleRedo={handleRedo} />  
