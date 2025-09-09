7b039129520d4b06cc5ff2c950a6eae0462d4e0a
unknown <giorgiapridonidze08@gmail.com>
2025-09-09 17:13:05 +0400

    added laytout for Versions.jsx to fill in future

--- src/containers/Versions.jsx
+++ src/containers/Versions.jsx
@@ -2,8 +2,22 @@ const Versions = () => {
     return(
     <div className="versions-container d-flex flex-column container  rounded-2 w-50 h-auto my-5 justify-content-center " style={{backgroundColor: localStorage.getItem('background-theme')}}>
         
-        <div className="v0.0.0-container">
+        <div className="v0.0.0-container container d-flex flex-column">
             <span className="fs-1">v0.0.0</span>
+
+            <span className="fs-3 ps-3">Features:</span>
+
+            <ul className="ps-5">
+                <li>...add content</li>
+            </ul>
+
+             <span className="fs-3 ps-3">Future Updates:</span>
+
+            <ul className="ps-5">
+                <li>...add content</li>
+            </ul>
+
+
         </div>
 
     </div>
